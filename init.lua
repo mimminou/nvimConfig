8dac0a75007a81196855578b6837728d4a155527
Amine Abdz <mimminou@gmail.com>
2025-11-13 08:59:27 +0100

    rebound 'r' in lspsaga finder to open in new buffer instead of tab, make indent 2 spaces wide

--- init.lua
+++ init.lua
@@ -1,45 +1,45 @@
 --neovim config
-vim.opt.shiftwidth = 4     -- indentation width
-vim.opt.tabstop = 4
+vim.opt.shiftwidth = 2 -- indentation width
+vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.clipboard = "unnamedplus" -- sync with system clipboard
-vim.opt.wrap = false --disable wrap by default
-vim.opt.undofile = true  -- enable infinite undo
-vim.opt.number = true        -- absolute line numbers
-vim.opt.relativenumber = true -- relative line numbers
+vim.opt.wrap = false              --disable wrap by default
+vim.opt.undofile = true           -- enable infinite undo
+vim.opt.number = true             -- absolute line numbers
+vim.opt.relativenumber = true     -- relative line numbers
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- diagnostics / visual settings
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = "●",  -- or "▎", "▶", etc
-    spacing = 4,
-  },
-  virtual_lines = false,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
+	virtual_text = {
+		prefix = "●", -- or "▎", "▶", etc
+		spacing = 4,
+	},
+	virtual_lines = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
 })
 
 -- settings for all lsp servers
 vim.lsp.config("*", {
-  root_markers = { ".git", "go.mod" },  -- tweak as needed
-  capabilities = {
-    textDocument = {
-      semanticTokens = {
-        multilineTokenSupport = true
-      },
-      completion = {
-        completionItem = {
-          snippetSupport = true
-        }
-      }
-    }
-  },
+	root_markers = { ".git", "go.mod" }, -- tweak as needed
+	capabilities = {
+		textDocument = {
+			semanticTokens = {
+				multilineTokenSupport = true
+			},
+			completion = {
+				completionItem = {
+					snippetSupport = true
+				}
+			}
+		}
+	},
 })
 
 --load plugins
--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,33 +1,36 @@
 return {
-  "nvimdev/lspsaga.nvim",
- -- event = "LspAttach",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter", -- optional, for better syntax in popups
-    "nvim-tree/nvim-web-devicons", -- optional, icons
-  },
-  config = function()
-    require("lspsaga").setup {
-      -- minimal setup, tweak as needed
-      lightbulb = {
-        enable = false,
-      },
-      finder = {
-        max_height = 0.5, -- 50% of screen
-      },
-      definition = {
-        max_height = 0.5,
-        keys = {
-          quit = "q",
-        },
-      },
-      rename = {
-        keys = {
-          quit = "q",
-        },
-      },
-      ui = {
-        border = "rounded",
-      },
-    }
-  end,
+	"nvimdev/lspsaga.nvim",
+	-- event = "LspAttach",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter", -- optional, for better syntax in popups
+		"nvim-tree/nvim-web-devicons",   -- optional, icons
+	},
+	config = function()
+		require("lspsaga").setup {
+			-- minimal setup, tweak as needed
+			lightbulb = {
+				enable = false,
+			},
+			finder = {
+				max_height = 0.5, -- 50% of screen
+				keys = {
+					toggle_or_open = 'r',
+				},
+			},
+			definition = {
+				max_height = 0.5,
+				keys = {
+					quit = "q",
+				},
+			},
+			rename = {
+				keys = {
+					quit = "q",
+				},
+			},
+			ui = {
+				border = "rounded",
+			},
+		}
+	end,
 }
