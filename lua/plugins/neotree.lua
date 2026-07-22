ce52659abab4c0eaeb2723ae31cfc5ce8543b942
Amine Abdz <mimminou@gmail.com>
2026-07-22 08:59:14 +0100

    fix neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -14,6 +14,20 @@ return {
           enabled = true,
           leave_dirs_open = false,
         },
+        window = {
+          fuzzy_finder_mappings = {
+            ["<CR>"] = function(state)
+              local node = state.tree:get_node()
+              if node and node.type == "file" then
+                require("neo-tree.sources.filesystem.commands").open(state)
+              end
+            end,
+            ["<down>"] = "move_cursor_down",
+            ["<C-n>"] = "move_cursor_down",
+            ["<up>"] = "move_cursor_up",
+            ["<C-p>"] = "move_cursor_up",
+          },
+        },
       },
       window = {
         position = "left",
